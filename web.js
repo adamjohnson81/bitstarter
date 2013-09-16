5405d703cdf5a5b8984e2ab56e617e4669839079
Adam Johnson <adamjohnson81@gmail.com>
2013-09-16 19:52:38 +0000

    index file

--- web.js
+++ web.js
@@ -1,2 +1,15 @@
-sponse.send(fs.readFile(index.html);
-..... [7G});
+var express = require('express');
+var app = express();
+fs = require('fs');
+var hello = fs.readfile(index.html)
+
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send(hello);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
