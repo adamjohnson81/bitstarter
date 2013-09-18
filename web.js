d5e921a3d0d296d19cf650b4b8de2268d855cc1e
Adam Johnson <adamjohnson81@gmail.com>
2013-09-18 02:28:13 +0000

    "more debugging"

--- web.js
+++ web.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
-fs = require('fs');
-var hello = fs.readfile(index.html);
+var fs = require('fs');
+var data = ();
+
+fs.readFile("index.html", "utf-8", function (err, data))
+{
+if (err) throw err;
+console.log(data.toString);
+});
 
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(hello);
+   response.send(console.log(data.toString);
 });
 
 var port = process.env.PORT || 5000;
