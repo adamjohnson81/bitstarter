e2661a87e3ce62b687a3bdc48b8337077b90bdae
Adam Johnson <adamjohnson81@gmail.com>
2013-09-18 19:38:39 +0000

    debug

--- web.js
+++ web.js
@@ -1,19 +1,17 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var data = ();
 
-fs.readFile("index.html", "utf-8", function (err, data))
-{
-if (err) throw err;
-console.log(data.toString);
-});
+var test = fs.readFileSync('./test.txt', 'utf-8');
+response.send (test);
+
+
 
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-   response.send(console.log(data.toString);
-});
+//app.get('/', function(request, response) {
+  // response.send('Hello world2');
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
